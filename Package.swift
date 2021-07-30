66546a775e9b9c72a5facaac673014924243c277
Jernej Zorec <328223+digitaljerry@users.noreply.github.com>
2021-07-30 16:05:28 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,5 +3,17 @@ import PackageDescription
 
 let package = Package(
     name: "AlamofireXMLRPC",
-    dependencies: []
+    platforms: [.iOS(.v10)],
+                      products: [.library(name: "AlamofireXMLRPC", targets: ["AlamofireXMLRPC"])],
+                      dependencies: [
+                          .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
+                          .package(url: "https://github.com/tadija/AEXML.git", from: "4.0.0")
+                      ],
+                      targets: [.target(name: "AlamofireXMLRPC",
+                                        dependencies: [
+                                            "Alamofire",
+                                            "AEXML"
+                                        ],
+                                        path: "Source")
+                               ]
 )
